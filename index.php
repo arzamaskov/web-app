80c93efd995dcfa2430e09961e9d8bf94c44a706
Andrey Arzamaskov <arzamaskov@gmail.com>
2020-03-04 07:26:25 +0400

    Web-App V2.0 : Get pictures from Instagram and display them to page

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 
 <?php
+    require_once 'display_errors.php';
     require_once 'vendor/autoload.php';
     use InstagramScraper\Instagram;
     
@@ -7,33 +8,24 @@
     $twig = new \Twig\Environment($loader);
 
     $instagram = new Instagram();
-    $account = $instagram->getAccount('camjpoulton');
 
-    // echo $twig->render('index.html', ['name' => 'Andrey']);
-    // $nonPrivateAccountMedias = $instagram->getMedias('arzamaskov');
-    // echo $twig->render('index.html', ['insta' => $nonPrivateAccountMedias[0]->getLink()]);
+    $url_list = array();
 
-    // Available fields
-    // echo "Account info:\n";
-    // echo "Id: {$account->getId()}\n";
-    // echo "Username: {$account->getUsername()}\n";
-    // echo "Full name: {$account->getFullName()}\n";
-    // echo "Biography: {$account->getBiography()}\n";
-    // echo "Profile picture url: {$account->getProfilePicUrl()}\n";
-    // echo "External link: {$account->getExternalUrl()}\n";
-    // echo "Number of published posts: {$account->getMediaCount()}\n";
-    // echo "Number of followers: {$account->getFollowsCount()}\n";
-    // echo "Number of follows: {$account->getFollowedByCount()}\n";
-    // echo "Is private: {$account->isPrivate()}\n";
-    // echo "Is verified: {$account->isVerified()}\n";
+    if ($_GET['tag']) {
+        $tag = $_GET['tag'];
+    }
+    else {
+        $tag = '';
+    }
 
-    $instaData = array(
-        'id' => $account->getId(),
-        'username' => $account->getUsername(),
-        'full_name' => $account->getFullName(),
-        'profile_pic' => $account->getProfilePicUrl(),
-        'followers' => $account->getFollowsCount(),
-        'follows' => $account->getFollowedByCount()
-    );
+    if ($tag == '') {
+        $error = 'Request cannot be emply. Try again.';
+    }
+    else {
+        $medias = $instagram->getMediasByTag($tag, 20);
+        foreach ($medias as $media) {
+            $url_list[]=$media->getImageHighResolutionUrl();
+        }
+    }
 
-    echo $twig->render('index.html', $instaData);
+    echo $twig->render('index.html', ['urls' => $url_list], 'tag => $tag');
