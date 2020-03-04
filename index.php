3699082af06148ee4338e768b125da5f487ef3e9
Andrey Arzamaskov <arzamaskov@gmail.com>
2020-03-04 23:18:54 +0400

    Web-App V2.0 : Modify index.php & index.html, fix bugs

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php
     require_once 'display_errors.php';
     require_once 'vendor/autoload.php';
@@ -6,26 +5,28 @@
 
     $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
     $twig = new \Twig\Environment($loader);
-
     $instagram = new Instagram();
 
     $urls = array();
 
-    if($_GET['tag'] == '') {
-        $tag = '';
+    if(isset($_GET['tag'])) {
+        $tag = $_GET['tag'];
+        if (trim($tag) == '') {
+            $error_warning = true;
+        }
+        else {
+            $medias = $instagram->getMediasByTag($tag, 20);
+            $media = $medias[0];
+            $error_warning = false;
+
+            foreach ($medias as $media) {
+                $urls[] = $media->getImageHighResolutionUrl();
+            }
+        }
+        
     }
     else {
-       $tag = $_GET['tag'];
-       $medias = $instagram->getMediasByTag($tag, 20);
-       $media = $medias[0];
-
-       foreach ($medias as $media) {
-           $urls[] = $media->getImageHighResolutionUrl();
-       }
-
+        $tag = '';
     }
 
-
-
-    echo $twig->render('index.html', ['urls' => $urls]);
-    //var_dump($urls);``
+    echo $twig->render('index.html', ['tag' => $tag, 'urls' => $urls, 'warning' => $error_warning]);
